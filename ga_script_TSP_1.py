905d5c5aeb7092dde6a109771efe4d554382163d
RitaMarques <anarita247@gmail.com>
2019-12-16 10:24:13 +0000

    ga

--- ga_script_TSP_1.py
+++ ga_script_TSP_1.py
@@ -3,9 +3,8 @@
 
 from cifo.algorithm.genetic_algorithm import GeneticAlgorithm
 from cifo.algorithm.hill_climbing import HillClimbing
-from cifo.custom_problem.knapsack_problem import (
-    KnapsackProblem, knapsack_decision_variables_example, knapsack_constraints_example, 
-    knapsack_bitflip_get_neighbors
+from cifo.custom_problem.travel_salesman_problem import (
+    TravelSalesmanProblem, tsp_decision_variables_example, knapsack_bitflip_get_neighbors # MUDAAAAAAAAAARR !!!!!!!!!!!!!
 )
 from cifo.problem.objective import ProblemObjective
 from cifo.algorithm.ga_operators import (
@@ -18,6 +17,7 @@ from cifo.algorithm.ga_operators import (
 from cifo.util.terminal import Terminal, FontColor
 from cifo.util.observer import GeneticAlgorithmObserver
 from random import randint
+import numpy as np
 
 def plot_performance_chart( df ):
     import plotly.graph_objects as go
@@ -92,20 +92,33 @@ def plot_performance_chart( df ):
 # Problem
 #--------------------------------------------------------------------------------------------------
 # Decision Variables
-dv = {
-    "Values"    : [360, 83, 59, 130, 431, 67, 230, 52, 93, 125, 670, 892, 600, 38, 48, 147, 
-    78, 256, 63, 17, 120, 164, 432, 35, 92, 110, 22, 42, 50, 323, 514, 28, 87, 73, 78, 15, 
-    26, 78, 210, 36, 85, 189, 274, 43, 33, 10, 19, 389, 276, 312], 
+input = [
+        [0, 2451, 713, 1018, 1631, 1374, 2408, 213, 2571, 875, 1420, 2145, 1972],
+        [2451, 0, 1745, 1524, 831, 1240, 959, 2596, 403, 1589, 1374, 357, 579],
+        [713, 1745, 0, 355, 920, 803, 1737, 851, 1858, 262, 940, 1453, 1260],
+        [1018, 1524, 355, 0, 700, 862, 1395, 1123, 1584, 466, 1056, 1280, 987],
+        [1631, 831, 920, 700, 0, 663, 1021, 1769, 949, 796, 879, 586, 371],
+        [1374, 1240, 803, 862, 663, 0, 1681, 1551, 1765, 547, 225, 887, 999],
+        [2408, 959, 1737, 1395, 1021, 1681, 0, 2493, 678, 1724, 1891, 1114, 701],
+        [213, 2596, 851, 1123, 1769, 1551, 2493, 0, 2699, 1038, 1605, 2300, 2099],
+        [2571, 403, 1858, 1584, 949, 1765, 678, 2699, 0, 1744, 1645, 653, 600],
+        [875, 1589, 262, 466, 796, 547, 1724, 1038, 1744, 0, 679, 1272, 1162],
+        [1420, 1374, 940, 1056, 879, 225, 1891, 1605, 1645, 679, 0, 1017, 1200],
+        [2145, 357, 1453, 1280, 586, 887, 1114, 2300, 653, 1272, 1017, 0, 504],
+        [1972, 579, 1260, 987, 371, 999, 701, 2099, 600, 1162, 1200, 504, 0],
+    ]
 
-    "Weights"   : [7, 0, 30, 22, 80, 94, 11, 81, 70, 64, 59, 18, 0, 36, 3, 8, 15, 42, 9, 0,
-    42, 47, 52, 32, 26, 48, 55, 6, 29, 84, 2, 4, 18, 56, 7, 29, 93, 44, 71,
-    3, 86, 66, 31, 65, 0, 79, 20, 65, 52, 13]
+tsp_decision_variables = {
+    "Distances" : np.array(input), #<< Number, Mandatory
+    "Item-Name" : [i for i in range(1,len(input[0]))] # << String, Optional, The names of the items, can be used to present the solution.
+        # It starts at 1 reserving the 0 for the static city
+        # We are setting the departure point, so we only have to arrange the n-1 remaining cities
 }
 
 # Problem Instance
-knapsack_problem_instance = KnapsackProblem( 
-    decision_variables = dv,
-    constraints = { "Max-Weight" : 400 })
+tsp_problem_instance = TravelSalesmanProblem(
+    decision_variables = tsp_decision_variables
+)
 
 
 # Configuration
