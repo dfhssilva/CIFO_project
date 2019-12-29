ccb20cafa97be2361c2c0e4046044765cd1141dd
DavidSilva98 <david.silva.silvestre@gmail.com>
2019-12-29 14:49:45 +0000

    Revert "visualizations"
    This reverts commit 986e9fbeda6dfab0fe220328f88f9c43347b446e.

--- scripts/export_local_aws.sh
+++ scripts/export_local_aws.sh
@@ -7,8 +7,8 @@ read -p "Path to store logs unix: " path_logs
 # Zip log_all
 zip log_all.zip ~/CIFO_project/log_all
 
-# Export data to local machine (run in local machine)
-# scp -i "$path_key_pair" ec2-user@"$public_dns":~/CIFO_project/log_all.zip "$path_logs"
+# Export data to local machine
+scp -i "$path_key_pair" ec2-user@"$public_dns":~/CIFO_project/log_all.zip "$path_logs"
 
-# Upload data from local machine (run in local machine)
+# Upload data from local machine
 # scp -i $path_key_pair requirements_aws.txt ec2-user@$public_dns:~/CIFO_project
