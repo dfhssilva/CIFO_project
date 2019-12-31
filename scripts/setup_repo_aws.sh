f2659a51cab75256fea23eec6585d085d72055c1
DavidSilva98 <david.silva.silvestre@gmail.com>
2019-12-31 16:49:51 +0000

    Update setup_repo_aws.sh

--- scripts/setup_repo_aws.sh
+++ scripts/setup_repo_aws.sh
@@ -2,6 +2,7 @@
 # Clone CIFO_project repository
 # git clone https://github.com/DavidSilva98/CIFO_project.git
 # cd CIFO_project
+# chmod +x ./scripts/*
 # Create and set-up environment
 python3 -m venv .env
 source .env/bin/activate
