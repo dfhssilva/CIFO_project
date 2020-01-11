202d1d5fbd86adebe7d4fe632f6e9d1a4d22ed07
pmfs-alves <pmfs.alves@gmail.com>
2020-01-11 14:50:50 +0000

    Update ga_script_TSP_data.py

--- ga_script_TSP_data.py
+++ ga_script_TSP_data.py
@@ -203,7 +203,8 @@ input = [
 
 tsp_decision_variables = {
     "Distances" : np.array(input), #<< Number, Mandatory
-    "Item-Name" : [i for i in range(1, len(input[0]))] # << String, Optional, The names of the items, can be used to present the solution.
+    "Item-Name" : [i for i in range(1, len(input[0]))]
+        # << String, Optional, The names of the items, can be used to present the solution.
         # It starts at 1 reserving the 0 for the static city
         # We are setting the departure point, so we only have to arrange the n-1 remaining cities
 }
@@ -237,8 +238,9 @@ valid_Replacement = {elitism_replacement: "elit", standard_replacement: "std"}
 #Parameters to grid search in a run
 test_init = [initialize_using_multiple, initialize_using_hc,  initialize_using_greedy, initialize_using_random]
 test_select = [tournament_selection, roulettewheel_selection, rank_selection]
-test_xover = [heuristic_crossover,, multiple_crossover,  cycle_crossover, pmx_crossover, order1_crossover]# singlepoint_crossover should not be used
-test_mutation = [multiple_mutation, inversion_mutation, multiple_mutation, greedy_mutation, swap_mutation, insert_mutation, scramble_mutation] # single_point_mutation should not be used
+test_xover = [heuristic_crossover, multiple_crossover,  cycle_crossover, pmx_crossover, order1_crossover]# singlepoint_crossover should not be used
+test_mutation = [multiple_mutation, inversion_mutation, multiple_mutation, greedy_mutation, swap_mutation,
+                 insert_mutation, scramble_mutation] # single_point_mutation should not be used
 test_replacement = [elitism_replacement, standard_replacement]
 
 test_xover_prob = [0.1, 0.9, 0.95, 0.05]
@@ -416,7 +418,7 @@ for init in range(len(test_init)):
                             else:
                                 one_combination()
 
-#[sum(sublist) for sublist in itertools.izip(*myListOfLists)]
+
 
 
 
